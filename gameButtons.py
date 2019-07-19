5fd74594e8242f7332502676610c03b08a5a446c
Angelika <Angelika>
2019-07-19 12:13:48 +0200

    created getField in gameButtons.py

--- gameButtons.py
+++ gameButtons.py
@@ -6,3 +6,6 @@ class gameButtons:
 
     def __init__(self):
         self.field =[[randint(0,4) for x in range(self.numberOfFields)] for y in range(self.numberOfFields)]
+    
+    def getField(self):
+        return self.field
