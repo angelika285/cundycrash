9862ba014e5028ebb9212c5246df74a0bd799e6a
Angelika <Angelika>
2019-07-19 12:52:36 +0200

    changed enum images

--- images.py
+++ images.py
@@ -1,13 +1,9 @@
 from enum import Enum
 
 class Images(Enum):
-    ANDROIDSTUDIO = 0
-    JAVA = 1
-    JETBRAINS = 2
-    PYTHON = 3
-    VSC = 4
-    #0 = "img/androidstudio.png"
-    #1 = "img/java.png"
-    #2 = "img/jetbrains.png"
-    #3 = "img/python.png"
-    #4 = "img/vsc.png"
+    ANDROIDSTUDIO = 0, "img/androidstudio.png"
+    JAVA = 1, "img/java.png"
+    JETBRAINS = 2, "img/jetbrains.png"
+    PYTHON = 3, "img/python.png"
+    VSC = 4, "img/vsc.png"
+    
