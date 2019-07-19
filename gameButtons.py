5203050d297ace478b84e3223e2f00ea6fda8f58
Angelika <Angelika>
2019-07-19 12:07:36 +0200

    initialized constructor for gameButtons.py

--- gameButtons.py
+++ gameButtons.py
@@ -1,2 +1,8 @@
+from random import*
+
 class gameButtons:
-    
+    numberOfFields = 9
+    field = None
+
+    def __init__(self):
+        self.field =[[randint(0,4) for x in range(self.numberOfFields)] for y in range(self.numberOfFields)]
--- hello.py
+++ hello.py
@@ -3,18 +3,15 @@ from array import *
 from tkinter import *
 import tkinter as tk
 from PIL import ImageTk, Image
+from random import *
 
 view = Tk()
 view.title("cundy crash")
 
-numberOfFields = 9
-
-field =[[0 for x in range(numberOfFields)] for y in range(numberOfFields)]
-
-for r in field:
-    for c in r:
-        print(c, end = " ")
-    print()
+#for r in field:
+#    for c in r:
+#        print(c, end = " ")
+#    print()
 
 b = Button(view, text = "hhhhhh")
 b.pack()
