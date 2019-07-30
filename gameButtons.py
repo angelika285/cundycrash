c8a47c2d6e4ea138cba81d1cf8343b9cb77cf5cf
Angelika <angelika.sudheimer@web.de>
2019-07-30 10:36:10 +0200

    changed methods check for right initialized row and column to one method

--- gameButtons.py
+++ gameButtons.py
@@ -8,8 +8,7 @@ class GameButtons:
 
     def __init__(self):
         self.field = np.random.randint(6, size=(self.numberOfColumns, self.numberOfRows))
-        self.checkForRightInitializedRow()
-        self.checkForRightInitializedColumn()
+        self.checkForRightInitialization()
     
     def getFields(self):
         return self.field
@@ -17,14 +16,24 @@ class GameButtons:
     def getField(self, col, row):
         return self.field[col, row]
 
-    def checkForRightInitializedRow(self):
+    def checkForRightInitialization(self):
         for row in range(0, self.field.shape[0]):
-            for column in range(0, self.field.shape[1]-2):
-                while self.field[row][column] == self.field[row][column+1] and self.field[row][column] == self.field[row][column+2]:
-                    self.field[row][column+2] = random.randint(0, 6)
-
-    def checkForRightInitializedColumn(self):
-        for row in range(0, self.field.shape[0]-2):
             for column in range(0, self.field.shape[1]):
-                while self.field[row][column] == self.field[row+1][column] and self.field[row][column] == self.field[row+2][column]:
-                    self.field[row+2][column] = random.randint(0, 6)
+                oldValue = None
+                if column < self.field.shape[1]-2 and self.areThreeInARow(row, column):
+                    oldValue = self.field[row][column]
+                    self.checkRow(row, column)
+                if row < self.field.shape[0]-2:
+                    self.checkColumn(row, column, oldValue)
+
+    def areThreeInARow(self, row, column):
+        return self.field[row][column] == self.field[row][column+1] and self.field[row][column] == self.field[row][column+2]
+
+    def checkRow(self, row, column):
+        while self.field[row][column] == self.field[row][column+1] and self.field[row][column] == self.field[row][column+2]:
+            self.field[row][column] = random.randint(0,5)
+
+    def checkColumn(self, row, column, oldValue):
+        while (self.field[row][column] == self.field[row+1][column] and self.field[row][column] == self.field[row+2][column]) or self.field[row][column] == oldValue:
+            self.field[row][column] = random.randint(0,5)
+    
